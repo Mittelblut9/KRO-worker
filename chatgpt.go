7cf8ee01379a65b2b4d7a60cc7d1e22f83533000
Mittelblut9 <sauer.benedikt@outlook.de>
2024-09-12 10:28:07 +0200

    fix

--- chatgpt.go
+++ chatgpt.go
@@ -68,13 +68,6 @@ func classify(transcription string, video api.Video) (Upcoming, error) {
 		fmt.Println("No choices found")
 		return Upcoming{}, nil
 	}
-	
-	err = json.Unmarshal([]byte(resp.Choices[0].Message.FunctionCall.Arguments), &data)
-	if err != nil {
-		fmt.Println("Error:", err)
-		fmt.Println("Response:", resp.Choices[0].Message.FunctionCall.Arguments)
-		return Upcoming{}, err
-	}
 
 	f, err := os.Create("chatgpt.json")
 	if err != nil {
